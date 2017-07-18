e48364958f3a2fc6054f6379e7d62ae8d41a753a
Joerg Stephan <johe.stephan@gmail.com>
2017-07-18 10:41:11 +0200

    Update tr.py

--- tr.py
+++ tr.py
@@ -20,21 +20,24 @@ for item in api.GetListTimeline("MYID"):
                 d1 = str(newD["urls"][0]["url"])
                 d2 = str(newD["text"])
                 d3 = str(newD["id_str"])
-                outfile.write('''%s: %s, %s\n''' % (d3, d2, d1))
-                #print('''%s: %s, %s\n''' % (d3, d2, d1))
-                r = requests.get(d1)
-                p1 = r.text
-                soup = BeautifulSoup(p1 , "html5lib")
-                pp1 = soup.get_text()
-                filename = "%s.txt" % d3
-                outfile1 = open(filename, "wb")
-                outfile1.write(bytes(pp1, 'UTF-8'))
-                outfile1.close()
-
-                for word in keywords:
-                        if word in pp1.lower():
-                                print("FOUND!: %s in %s" % (word,d3))
-
+                filename = "./%s.txt" % d3
+		            print('''%s: %s, %s''' % (d3, d2, d1))
+		              if not os.path.exists(filename):
+			              outfile.write('''%s: %s, %s\n''' % (d3, d2, d1))
+			              #print('''%s: %s, %s\n''' % (d3, d2, d1))
+                    r = requests.get(d1)
+                    p1 = r.text
+                    soup = BeautifulSoup(p1 , "html5lib")
+                    pp1 = soup.get_text()	
+                    outfile1 = open(filename, "wb")
+                    outfile1.write(bytes(pp1, 'UTF-8'))
+                    outfile1.close()	
+		
+                    for word in keywords:
+                      if word in pp1.lower():
+                        print("FOUND!: %s in %s\n\n" % (word,d3))
+                  else:
+			              print("%s exists\n\n" % filename)
         except: 
                 print(item)
                 continue
